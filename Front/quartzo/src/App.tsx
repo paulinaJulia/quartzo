bff7310e2b9a37dfdb4e6ca36383636579e14937
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-12-25 15:39:03 -0300

    <feat>: importando funcionalidades do React para tela de login

---
+++ Front/quartzo/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import { Signin } from "./components/Signin";
+
+function App() {
+    return (
+        <div>
+            <Signin />
+        </div>
+    );
+}
+export default App;
