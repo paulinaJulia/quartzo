3ae514859d23547818a7f6aede91931b5fd2757a
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-01-02 23:53:25 -0300

    <feat>: add estilo e centralização dos conainers

--- Front/quartzo/src/components/Signin/index.tsx
+++ Front/quartzo/src/components/Signin/index.tsx
@@ -1,21 +1,21 @@
 import React from "react"
 import { Container, Form, Logo } from "./styles";
-import { Button, FilledInput, InputAdornment } from "@mui/material";
+import FilledInput from "@mui/material/FilledInput";
+import { Button, InputAdornment } from "@mui/material";
 import { HiOutlineUser } from "react-icons/hi"
 import { SlLock } from "react-icons/sl"
-
 
 export const Signin = () =>{
     return(
         <Container>
-
            <div className="container">
             <div className="container1">CONTAINER 1</div>
                 <div className="imagem">
-                    <img src="logo612.png" 
+                    <img 
+                    src="logo_quartzo.png" 
                     width={400}
                     height={400}
-                    className="imagem"
+                    className="img-banner"
                     alt="banner" />
                 </div>
             <div className="container2">
@@ -32,7 +32,7 @@ export const Signin = () =>{
                                     <HiOutlineUser 
                                     style={{
                                         color: "#0563e7ba",
-                                        fontSize: "1.5rem",
+                                        fontSize: "1.3rem",
                                     }}
                                     />
                                 </InputAdornment>
@@ -49,22 +49,27 @@ export const Signin = () =>{
                                     <SlLock
                                     style={{
                                         color: "#0563e7ba",
-                                        fontSize: "1.5rem",
+                                        fontSize: "1.3rem",
                                     }}
                                     />
                                 </InputAdornment>
                             }
                         />
-                        <div className="buttom-checkbox">
+                        <div className="buttom">
                             <Button variant="contained" className="buttom">
                                 Login
                             </Button>
-                                <div>
+                                <div className="checkbox">
                                     <span>
                                         <input type="checkbox" className="input-checkbox" />
                                     </span>
                                     <p>Lembrar senha.</p>
                                 </div>
+                        </div>
+                        <div className="cad">
+                            <p>
+                                Criar conta <a href="/">Cadastrar.</a>
+                            </p>
                         </div>
                 </Form>
             </div>
--- Front/quartzo/src/components/Signin/styles.ts
+++ Front/quartzo/src/components/Signin/styles.ts
@@ -1,56 +1,201 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const moveToRight = keyframes`
+
+0% {
+  opacity: 0;
+  transform: translate(-200px);
+
+}
+50%{
+  opacity: 3;
+}
+100%{
+
+  transform: translate(0px);
+  opacity: 1;
+}
+
+
+`;
 
 export const Container = styled.div`
-    .container{
-        width: 100%;
-        height: 100vh;
-        display: flex;
-        flex-direction: row;
-    }
+  width: 100%;
+  height: 100vh;
+  .container {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    width: 100%;
+  }
 
+  .container1 {
+    width: 60%;
+    background-image: radial-gradient(
+      circle at 50% 50%,
+      #8a9cff 0,
+      #4876e5 50%,
+      #00439b 100%
+    );
+    box-shadow: 1px 2px 3px #ccc;
+    height: 100vh;
+  }
+  @media (max-width: 999px) {
     .container1 {
-        width: 50%;
-        background-image: radial-gradient(
-            circle at 50% 50%,
-            #8a9cff 0,   
-            #4876e5 50%,
-            #00439b 100%
-        );
-        box-shadow: 1px 2px 3px #ccc;
-        height: 100vh;
+      display: none;
     }
-    .image{
-        margin: auto;
-        align-items: center;
-        justify-content: center;
-        display: flex; width: 100%;
-        height: 100%;
-    }
-
+  }
+  .image {
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    animation: ${moveToRight} 0.5s;
+  }
+  .container2 {
+    width: 40%;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    height: 100vh;
+    height: 100%;
+  }
+  @media (max-width: 999px) {
     .container2 {
-        width: 50%;
-        display: flex
-        align-items: center;
-        margin: auto;
-        // background: red;
-        height: 100vh;
+      width: 90%;
     }
+  }
 `;
 
 export const Form = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 50vh;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 50vh;
+  align-items: center;
+  margin: auto;
+
+  .input {
+    width: 60%;
+    justify-content: center;
     margin: auto;
-    .input{
-        width: 50%;
-        justify-content: center;
-        margin auto;
-        background: #d3d3d3;
-        margin-buttom: 1rem;
+    background: #fff;
+    margin-bottom: 1rem;
+    animation: ${moveToRight} 0.5s;
+  }
+  @media (max-width: 999px) {
+    width: 100%;
+    height: 65vh;
+    background-image: radial-gradient(
+      circle at 50% 50%,
+      #486ee5 0,
+      #486ee5 50%,
+      #00439b 100%
+    );
+    border-radius: 30px;
+    .input {
+      width: 80%;
+      animation: ${moveToRight} 0.5s;
     }
+  }
+  .checkbox {
+    align-items: center;
+    display: flex;
+    margin: auto;
+    flex-direction: row;
+    width: 100%;
+    justify-content: end;
+    animation: ${moveToRight} 0.5s;
+    p {
+      font-size: 1.2rem;
+      color: #cccc;
+    }
+  }
+  .input-checkbox {
+    margin-right: 5px;
+    margin-bottom: -4px;
+  }
+  .bottom {
+    width: 60%;
+    margin-top: 1.3rem;
+    justify-content: space-between;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  @media (max-width: 999px) {
+    .bottom {
+      width: 80%;
+    }
+    .buttom {
+      background: #fc750f;
+    }
+    .checkbox p {
+      color: #fff;
+    }
+  }
+  .buttom {
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    padding: 0 30px;
+    height: 40px;
+  }
+  .cad {
+    margin-top: 1.2rem;
+    p {
+      font-size: 1.2rem;
+      color: #cccc;
+    }
+    a {
+      color: #486ee5;
+    }
+  }
+  @media (max-width: 999px) {
+    .cad {
+      width: 85%;
+      gap: 1rem;
+      margin-top: 1rem;
+
+      p {
+        font-size: 1rem;
+        margin-top: 1rem;
+        text-align: center;
+        color: #fff;
+      }
+      a {
+        font-weight: 500;
+        color: #fc750f;
+      }
+    }
+  }
 `;
 
-export const Logo = styled.div``;
+export const Logo = styled.div`
+  font-family: "Poppins", sans-serif;
+  width: 60%;
+  font-size: 2rem;
+  font-weight: 400;
+  color: #486ee5;
+  margin: auto;
+  justify-content: center;
+
+  p {
+    margin-top: -1rem;
+    animation: ${moveToRight} 0.5s;
+  }
+  @media (max-width: 999px) {
+    width: 100%;
+    margin: auto;
+    justify-content: center;
+
+    p {
+      text-align: center;
+      font-size: 2.5rem;
+      margin-top: 0;
+      animation: ${moveToRight} 0.5s;
+      color: #fff;
+    }
+  }
+`;
