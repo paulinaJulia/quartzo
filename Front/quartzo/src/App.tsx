270f1751627ac6aa1faa0c8dad920fdc71f36f9e
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-01-04 16:06:26 -0300

    <chore>: adicionando signup ao index.tsx. para aplicação posterior no components

--- Front/quartzo/src/App.tsx
+++ Front/quartzo/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Signin } from "./components/Signin";
+import { Signup } from "./components/Signup";
 
 function App() {
     return (
         <div>
             <Signin />
+            <Signup />
         </div>
     );
 }
---
+++ Front/quartzo/src/components/Signup/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+// import { Container, Form } from "./styles";
+
+export const Signup
