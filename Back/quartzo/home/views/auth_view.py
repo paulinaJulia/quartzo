1db63cbf752e5dce191b88f88ebf5da01e641fcf
Dalton Campos <dalton.campos20@gmail.com>
2025-01-06 21:51:54 -0300

    feat: Adiciona rotas de login e register

---
+++ Back/quartzo/home/views/auth_view.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from ..serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Usuário criado com sucesso!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    @csrf_exempt
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({"error": "Credenciais inválidas!"}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    
