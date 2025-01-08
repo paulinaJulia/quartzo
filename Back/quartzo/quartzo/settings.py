485b0c4b947caa6357e9a977d36d1a7dd068374c
Dalton Campos <dalton.campos20@gmail.com>
2025-01-07 22:36:23 -0300

    feat: Adiciona lib para tratar cros

--- Back/quartzo/quartzo/settings.py
+++ Back/quartzo/quartzo/settings.py
@@ -22,6 +22,8 @@ INTERNAL_IPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:9000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 INSTALLED_APPS = [
@@ -34,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
+    'corsheaders',
     #
     # Apps
     "avatar",
@@ -58,6 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
