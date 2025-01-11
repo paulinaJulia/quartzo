71a4daf56ee1a95479e29889da65c2fc8390cf29
paulinaJulia <paulinajulia2015@gmail.com>
2025-01-11 15:03:58 -0300

    refactor: Ajuste na escrita para string em__init__.py

--- Back/quartzo/financeiro/admin/__init__.py
+++ Back/quartzo/financeiro/admin/__init__.py
@@ -2,6 +2,6 @@ from .contrato_admin import ContratoAdmin
 from .pagamento_admin import PagamentoAdmin
 
 __all__ = [
-    ContratoAdmin,
-    PagamentoAdmin,
+    'ContratoAdmin',
+    'PagamentoAdmin',
 ]
