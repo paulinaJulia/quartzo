c4b01df320a3a0838ac502287e06f15e5917413c
paulinaJulia <65190211+paulinaJulia@users.noreply.github.com>
2025-01-14 22:13:06 -0300

    Update index.tsx

--- Front/quartzo/src/components/Signup/index.tsx
+++ Front/quartzo/src/components/Signup/index.tsx
@@ -8,7 +8,7 @@ export const Signup: React.FC = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
-    const BASE_URL = process.env.REACT_APP_API_URL || "https://teste-paulina.novadata.dev/";
+    const BASE_URL = process.env.REACT_APP_API_URL || "";
 
     const handleSignup = async (e: React.FormEvent) => {
         e.preventDefault();
